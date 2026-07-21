c48be0a0cf9fb78242f98ce8e9087135445c2434
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-07-21 12:37:00 +0300

    creat tabel 1 priors

--- Specification-System/modelbuild/ModelBuilding.cs
+++ Specification-System/modelbuild/ModelBuilding.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿//using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace Specification_System.modelbuild
 {
-    internal class ModelBuilding
+    public class ModelBuilding
     {
+        public void Train(List<Dictionary> rows, string targetColumn)
+        {
+            int n = rows.Count();
+            var labels = rows.Select(row => row[targetColumn]).Distinct();
+
+            Dictionary<string, double> priors = new Dictionary<string, double>();
+
+            foreach (string each in labels)
+            {
+                double priors = rows.Where(g => g[targetColumn] == each).Count();
+                priors[each} = count / n;
+            }
+        }
     }
 }
+
+
+
+
+
+
