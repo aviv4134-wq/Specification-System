5c77a92df9211ae649766b0e026f1a882f4476cd
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-07-21 16:46:46 +0300

    add model

--- Specification-System/modelbuild/ModelBuilding.cs
+++ Specification-System/modelbuild/ModelBuilding.cs
@@ -1,5 +1,6 @@
-﻿//using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.SymbolStore;
 using System.Reflection.Emit;
 using System.Text;
@@ -8,21 +9,54 @@ namespace Specification_System.modelbuild
 {
     public class ModelBuilding
     {
-        public void Train(List<Dictionary> rows, string targetColumn)
-        {
+        public Tuple<List<string>, Dictionary<string, double>, Dictionary<Tuple<string, string, string>, double>, Dictionary<Tuple<string, string>, double>>
+            Train(List<Dictionary<string, string>> rows, string targetColumn)
+            {
+            
             int n = rows.Count();
-            var labels = rows.Select(row => row[targetColumn]).Distinct();
-
+            var labels = rows.Select(row => row[targetColumn]).Distinct().ToList();
+            //A
             Dictionary<string, double> priors = new Dictionary<string, double>();
 
             foreach (string each in labels)
             {
-                double priors = rows.Where(g => g[targetColumn] == each).Count();
-                priors[each} = count / n;
+                double count = rows.Where(g => g[targetColumn]?.ToString() == each).Count();
+
+                priors[each] = count / n;
             }
+            //B
+            Dictionary<Tuple<string, string, string>, double> cond = new Dictionary<Tuple<string, string, string>, double>();
+            Dictionary<Tuple<string, string>, double> unseen = new Dictionary<Tuple<string, string>, double>();
+
+            foreach(string label in labels)
+            {
+                var labelRows = rows.Where(row => row[targetColumn]?.ToString() == label).ToList();
+                int countLabelRows = labelRows.Count;
+
+                var featureColumns = rows.First().Keys.Where(col => col != targetColumn);
+
+                foreach (string feature in featureColumns)
+                {
+                    var distinctValues = rows.Select(row => row[feature]).Distinct().ToList();
+                    int distinctCount = distinctValues.Count();
+
+                    foreach (string val in distinctValues)
+                    {
+                        int matchCount = labelRows.Count(row => row[feature]?.ToString() == val);
+                        double probability = (matchCount + 1.0) / (countLabelRows + distinctCount);
+                        cond[Tuple.Create(label, feature, val)] = probability;
+                        Console.WriteLine(cond);
+                    }
+                    unseen[Tuple.Create(label, feature)] = 1.0 / (countLabelRows + distinctCount);
+                }
+            }
+            return Tuple.Create(labels, priors, cond, unseen);
         }
+
     }
+    
 }
+
 
 
 
