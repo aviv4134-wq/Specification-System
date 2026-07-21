5f781fa20797c04d35d0a469a3bddee8ba4dad74
avraham <aviv4134@gmail.com>
2026-07-21 11:03:42 +0300

    add to mergeing

--- Specification-System/Program.cs
+++ Specification-System/Program.cs
@@ -13,7 +13,7 @@ namespace Specification_System
             InputCsv lo = new InputCsv();
             string[] rows = lo.load();
 
-            Console.WriteLine(lo.loadRows(rows)[^1]); 
+            
         }
     }
 }
--- Specification-System/output/SaveToCsv.cs
+++ Specification-System/output/SaveToCsv.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Specification_System.output
-{
-    internal class SaveToCsv
-    {
-    }
-}
+//namespace Specification_System.output
+//{
+//    class SaveToCsv : Ioutput
+//    {
+//        public void Output()
+//        {
+
+//        }
+//    }
+//}
