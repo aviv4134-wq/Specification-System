d13465ea79ce8a49c3bf070ef7d12651884c9d07
avraham <aviv4134@gmail.com>
2026-07-22 23:07:08 +0300

    ref:add constractor classification

--- Specification-System/classifications/BatchClassification.cs
+++ Specification-System/classifications/BatchClassification.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualBasic;
+using Specification_System.modelbuild;
+using Specification_System.output;
 using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
@@ -8,6 +10,9 @@ namespace Specification_System.classifications
 {
     class BatchClassification : Classification
     {
+
+        public BatchClassification(Ioutput output, ModelBuilding builder) : base(output, builder) 
+        { }
          public List<Dictionary<string, string>> Execute(List<Dictionary<string, string>> allRows, Tuple<List<string>, Dictionary<string, double>, Dictionary<Tuple<string, string, string>, double>, Dictionary<Tuple<string, string>, double>> model,string label)
         {
             for (int index = 0;index < allRows.Count; index++)
@@ -17,7 +22,7 @@ namespace Specification_System.classifications
                 string labelResoult = modelBuilder.Predict(model, row);
                
                 row[label] = labelResoult;
-                consule.Output($"row {rowNumber}:{row.Keys} -> {labelResoult}  ");
+                outputOption.Output($"row {rowNumber}:{row.Keys} -> {labelResoult}  ");
             }
             return allRows;
         }
--- Specification-System/classifications/InteractiveClassification.cs
+++ Specification-System/classifications/InteractiveClassification.cs
@@ -9,7 +9,8 @@ namespace Specification_System.classifications
 {
      class InteractiveClassification : Classification
     {
-        
+        public InteractiveClassification (Ioutput output, ModelBuilding builder) : base(output, builder)
+        { }
 
         public void Execute(List<Dictionary<string, string>> allRows, Tuple<List<string>, Dictionary<string, double>, Dictionary<Tuple<string, string, string>, double>, Dictionary<Tuple<string, string>, double>> model) //מקבל מודל אם כול השורות של הקבוץ הבונה כדי להשתמש בדיקט
         {
@@ -17,7 +18,8 @@ namespace Specification_System.classifications
             foreach (Dictionary<string, string> row in allRows)
             {
                 string labelResoult = modelBuilder.Predict(model, row);
-                consule.Output(labelResoult);
+                
+                outputOption.Output(labelResoult);
             }
         }
     }
